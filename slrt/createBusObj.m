f1af9ebe3aca77d9424e2a3d3fa4ba0e49aa6885
Johannes Spinneken <johannes@evergreeninnovations.co>
2024-04-11 16:05:37 -0700

    Next code iteration

---
+++ slrt/createBusObj.m
@@ -0,0 +1,49 @@
+
+
+%% === EtherCAT status bus ================================================
+clear eCatStatusStruct;
+eCatStatusStruct.eCatErr = int32(0);
+eCatStatusStruct.eCatLastErr = int32(0);
+eCatStatusStruct.eCatState = int32(0);
+eCatStatusStruct.eCatDcErr = int32(0);
+eCatStatusStruct.eCatMnClkDiff = int32(0);
+eCatStatusStruct.eCatDcInit = int32(0);
+eCatStatusStruct.eCatNsClkDiff = int32(0);
+
+eCatStatusBusInfo = Simulink.Bus.createObject(eCatStatusStruct);
+eCatStatusBus = evalin('base',eCatStatusBusInfo.busName);
+% =========================================================================
+
+%% ==== Actuator status bus ===============================================
+clear actuatorStatusStruct;
+
+actuatorStatusStruct.opEnabled = logical(false);
+actuatorStatusStruct.homed = logical(false);
+actuatorStatusStruct.warning = logical(false);
+actuatorStatusStruct.error = logical(false);
+
+actuatorStatusStruct.position_m = 0.0;
+actuatorStatusStruct.velocity_m_s = 0.0;
+actuatorStatusStruct.force_N = 0.0;
+
+actuatorStatusBusInfo = Simulink.Bus.createObject(actuatorStatusStruct);
+actuatorStatusBus = evalin('base',actuatorStatusBusInfo.busName);
+% =========================================================================
+
+%% === Control bus ========================================================
+clear ctrlStruct;
+
+ctrlStruct.state = uint32(0); % internal state
+ctrlStruct.switchOn = logical(false);
+ctrlStruct.home = logical(false);
+ctrlStruct.specialMode = logical(false);
+ctrlStruct.acknError = logical(false);
+
+ctrlStruct.current_A = 0.0; % might change to vel ctrl?
+
+ctrlBusInfo = Simulink.Bus.createObject(ctrlStruct);
+ctrlBus = evalin('base',ctrlBusInfo.busName);
+% =========================================================================
+
+
+
--- slrt/init.m
+++ slrt/init.m
@@ -2,7 +2,26 @@
 
 clearvars; close all; clc;
 
+
+%% === constants ==========================================================
+createBusObj;
+createDefinitions;
+
 Ts = 1/1000;    % SLRT loop rate
+
+tgName = 'EGIBaseline1';
+
+mdlName = 'osuWorkshop';
+appFileName = 'workshopApp';
+
+fileDecimation = 5; % 5 -> 200Hz at 1kHz Fs
+
+appDecimation = 200; % decimation of app (200 = 5Hz at 1kHz Fs)
+appDecimationReduced = 1000;
+appDecimationAxes = 100;
+
+slBuildDir = 'c:\simulink\build2024a'; % built directory; this is where the App gets the RT excecutable from
+slCacheDir = 'c:\simulink\cache2024a'; % cache directory
 
 lb2N = 4.44822;
 lodCellMaxLb = 100;
@@ -16,3 +35,96 @@ el3751DigitalRange = double(0x773594); % corresponds to 32 mV/V
 el3751Scale = el3751DigitalRange / el3751PhysicalRange; % reading per mV/V
 
 loadCellScale = loadCellMaxN/(el3751Scale*loadCellCal); % from digital reading to N
+% =========================================================================
+
+%% === Setup SDI ==========================================================
+Simulink.sdi.clear;                     % clear old data from SDI
+Simulink.sdi.setRecordData(true);       % needs to be true to enable later data import
+Simulink.sdi.setAutoArchiveMode(false); % set archive to false to avoid SDI DB growing
+% =========================================================================
+
+%% == set build/cache paths ===============================================
+if ~strcmpi(get_param(0,'CacheFolder'),slCacheDir) || ...
+        ~strcmpi(get_param(0,'CodeGenFolder'),slBuildDir)
+    resp = questdlg({['Simulink build and cache folders not set to ', ...
+                        slBuildDir,' and ',slCacheDir,'.'],...
+                    'Would you like to set these now?', ...
+                    'Disregarding may result in unintended behavior.'}, ...
+                    'Simulink build settings','Yes');
+
+    if isempty(resp) || strcmp(resp,'Cancel')
+        disp('Stopping build')
+        return
+    end
+
+    if strcmp(resp,'Yes')
+        if ~exist(slCacheDir,'dir')
+            mkdir(slCacheDir);
+        end
+        if ~exist(slBuildDir,'dir')
+            mkdir(slBuildDir);
+        end
+        set_param(0,'CacheFolder',slCacheDir)
+        set_param(0,'CodeGenFolder',slBuildDir)
+    end
+end
+% =========================================================================
+
+%% === build code =========================================================
+resp = questdlg({'Would you like to rebuild the model?'},'Perform Build?','Yes');
+
+if isempty(resp) || strcmp(resp,'Cancel')
+    disp('InitCtrl cancelled by user.')
+    return
+end
+
+if strcmp(resp,'Yes')
+   
+    fprintf('*** Build Simulink RT (Speedgoat) code ...\n\n')
+    load_system(mdlName);
+    set_param(mdlName, 'RTWVerbose', 'off');
+  
+    % build model
+    slbuild(mdlName)
+
+    % === setting app options, see https://www.mathworks.com/matlabcentral/answers/1813580-why-does-it-take-so-long-to-stop-a-real-time-application-running-on-speedgoat-if-my-model-contains-m
+    rtApp = slrealtime.Application(mdlName);
+    updateAutoSaveParameterSetOnStop(rtApp,false);
+
+else
+    fprintf('*** Skipping Simulink Build Proceedure. Using cached model.\n\n')
+end
+% =========================================================================
+
+%% === create a timestamped copy of the model =============================
+currentTime = datetime;
+currentTime.Format = 'yyyy_MM_dd_HH_mm_ss';
+timeStr = char(currentTime); 
+mdlNameTs = [mdlName,'_',timeStr];
+
+src = fullfile(slBuildDir,[mdlName,'.mldatx']);
+dest = fullfile(slBuildDir,[mdlNameTs,'.mldatx']);
+
+copyfile(src,dest);
+fprintf('Copied model to %s.\n',dest)
+% =========================================================================
+
+%% === test Speedgoat connection ==========================================
+tg = slrealtime(tgName);
+try 
+   tg.connect
+catch ME
+   fprintf('\n*** Target %s not connected. Stopping program. Check connection.\n',tgName)
+   fprintf('\n*** Matlab error \n %s \n\n',ME.getReport)   
+   return  
+end
+
+if tg.isConnected 
+   fprintf('\n*** Target %s is connected at IP address %s. Starting app ...\n\n',tg.TargetSettings.name,tg.TargetSettings.address)
+end
+% ========================================================================= 
+
+%% === start the app ======================================================
+run(appFileName)
+% =========================================================================  
+
